fcfab7da98c094fee7ecac1cafcff9001e2d2be0
jleonen <jleonen2012@gmail.com>
2021-11-07 19:35:17 -0800

    Re-commit

--- Social Media Dashboard/app.js
+++ Social Media Dashboard/app.js
@@ -8,19 +8,29 @@ const dashboardHeading = document.querySelector(".heading h2");
 const overviewHeading = document.querySelector(".overview-title");
 const header = document.querySelector("header");
 
-const toggleCardBackground = () => {
+//change background color of specific elements
+const toggleBackgroundColor = () => {
+  //change bg color of overview cards
   overviewItems.forEach((item) => {
     item.classList.toggle("dark-items");
   });
+  //change bg color of dashboard cards
   dashboardItems.forEach((item) => {
     item.classList.toggle("dark-items");
   });
+  //change color of website background
+  siteBG.classList.toggle("dark-bg");
+  //change color of header background
+  header.classList.toggle("dark-header");
+};
+
+//change font color of dashboard and overview titles
+const toggleFontColor = () => {
+  dashboardHeading.classList.toggle("dark-dashboard");
+  overviewHeading.classList.toggle("dark-overview");
 };
 
 toggleMode.addEventListener("click", function () {
-  siteBG.classList.toggle("dark-bg");
-  dashboardHeading.classList.toggle("dark-dashboard");
-  overviewHeading.classList.toggle("dark-overview");
-  header.classList.toggle("dark-header");
-  toggleCardBackground();
+  toggleBackgroundColor();
+  toggleFontColor();
 });
