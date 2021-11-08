a3f78c3148e52c9afee3f215a643a9aff0659a13
jleonen <jleonen2012@gmail.com>
2021-11-07 19:34:02 -0800

    Refactored toggle logic into specific functions: toggleBackgroundColor for changing background color and toggleFontColor to change font color

--- Social Media Dashboard/app.js
+++ Social Media Dashboard/app.js
@@ -8,15 +8,19 @@ const dashboardHeading = document.querySelector(".heading h2");
 const overviewHeading = document.querySelector(".overview-title");
 const header = document.querySelector("header");
 
-toggleMode.addEventListener("click", function () {
-  siteBG.classList.toggle("dark-bg");
-  dashboardHeading.classList.toggle("dark-dashboard");
-  overviewHeading.classList.toggle("dark-overview");
-  header.classList.toggle("dark-header");
+const toggleCardBackground = () => {
   overviewItems.forEach((item) => {
     item.classList.toggle("dark-items");
   });
   dashboardItems.forEach((item) => {
     item.classList.toggle("dark-items");
   });
+};
+
+toggleMode.addEventListener("click", function () {
+  siteBG.classList.toggle("dark-bg");
+  dashboardHeading.classList.toggle("dark-dashboard");
+  overviewHeading.classList.toggle("dark-overview");
+  header.classList.toggle("dark-header");
+  toggleCardBackground();
 });
